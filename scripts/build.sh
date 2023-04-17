4005bda17ba18febd4956ee3ee4578342708962b
toka.fawy <toka.fawy@gmail.com>
2023-04-17 14:26:27 +0200

    test

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+NETWORK_NAME=sharding
+if [ -z $(docker network ls --filter name=^${NETWORK_NAME}$ --format="{{ .Name }}") ] ; then 
+     docker network create ${NETWORK_NAME} ; 
+fi
+
+docker compose -f docker-compose.yaml up -d
